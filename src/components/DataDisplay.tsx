0113c922bdace212a9d21863f75ba95099601b51
DilmaTT <ttcugen3@gmail.com>
2025-08-19 12:07:54 +0700

    Initial commit

--- src/components/DataDisplay.tsx
+++ src/components/DataDisplay.tsx
@@ -1,24 +1,9 @@
 import ListView from './ListView';
-import CalendarView from './CalendarView';
-import { useStorage } from '@/hooks/useStorage'; // Keep useStorage
 
 const DataDisplay = () => {
-  const { settings } = useStorage();
-
-  const renderView = () => {
-    switch (settings.view) {
-      case 'list':
-        return <ListView />;
-      case 'calendar':
-        return <CalendarView />;
-      default: // Handle 'custom' or any other unexpected value by defaulting to list
-        return <ListView />;
-    }
-  };
-
   return (
     <div className="w-full">
-      {renderView()}
+      <ListView />
     </div>
   );
 };
